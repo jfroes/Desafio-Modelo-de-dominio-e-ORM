a7d83f0729607572246f0d4395ed39fed2f9c20c
José Froes <josepaulo.froes@gmail.com>
2025-07-05 14:29:40 -0300

    :tada: first commit

---
+++ src/main/java/com/devsuperior/desafio_modelo_de_dominio_orm/DesafioModeloDeDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio_modelo_de_dominio_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioModeloDeDominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioModeloDeDominioOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/desafio_modelo_de_dominio_orm/DesafioModeloDeDominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafio_modelo_de_dominio_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioModeloDeDominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
